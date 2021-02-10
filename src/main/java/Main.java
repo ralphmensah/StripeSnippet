d96ba2b3b0465f9bc0be320166965972c625eb45
ralphmensah <ralphmensah123@gmail.com>
2021-02-10 08:40:29 +0000

    Update Main.java
    Fixed commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.PaymentIntent;
+import com.stripe.model.*;
+import com.stripe.param.CustomerRetrieveParams;
 import com.stripe.param.PaymentIntentConfirmParams;
 import com.stripe.param.PaymentIntentCreateParams;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws StripeException {
@@ -11,92 +15,92 @@ public class Main {
         Stripe.apiKey =MY_API_KEY;
         //work on stripe here
 
-//        Map<String ,Object> customerParams = new HashMap<>();
-//        customerParams.put("email","man@mail.com");
+        Map<String ,Object> customerParams = new HashMap<>();
+        customerParams.put("email","man@mail.com");
 //        Created a customer
-//        Customer customer = Customer.create(customerParams);
-//        System.out.println(customer.toJson());
-//        System.out.println("Customer id :" + customer.getId());
+        Customer customer = Customer.create(customerParams);
+        System.out.println(customer.toJson());
+        System.out.println("Customer id :" + customer.getId());
 
 //        retrieve customer
-//        CustomerRetrieveParams CustomerParams = CustomerRetrieveParams.builder()
-//                .addExpand("sources").build();
-//        Customer a = Customer.retrieve("cus_Iu6pfP565PDEN6", CustomerParams, null);
+        CustomerRetrieveParams CustomerParams = CustomerRetrieveParams.builder()
+                .addExpand("sources").build();
+        Customer a = Customer.retrieve("cus_Iu6pfP565PDEN6", CustomerParams, null);
 //        outdated
 //        Customer a  = Customer.retrieve("cus_Iu6pfP565PDEN6");
-//        System.out.println("Customer  for a :" + a.toJson());
+        System.out.println("Customer  for a :" + a.toJson());
 
 //        adding a card
-//        Map<String ,Object> cardParams = new HashMap<>();
-//        cardParams.put("number","4242424242424242");
-//        cardParams.put("exp_month","11");
-//        cardParams.put("exp_year","23");
-//        cardParams.put("cvc","123");
+        Map<String ,Object> cardParams = new HashMap<>();
+        cardParams.put("number","4242424242424242");
+        cardParams.put("exp_month","11");
+        cardParams.put("exp_year","23");
+        cardParams.put("cvc","123");
+
+//        creating token for secure payment
+        Map<String ,Object> tokenParams = new HashMap<>();
+        tokenParams.put("card",cardParams);
 //
-////        creating token for secure payment
-//        Map<String ,Object> tokenParams = new HashMap<>();
-//        tokenParams.put("card",cardParams);
-////
-//        Token token = Token.create(tokenParams);
-//
-////        Adding source
-//        Map<String ,Object> source = new HashMap<>();
-//        source.put("source", token.getId());
-//
-////        Adding source to customer
-//            a.getSources().create(source);
+        Token token = Token.create(tokenParams);
+
+//        Adding source
+        Map<String ,Object> source = new HashMap<>();
+        source.put("source", token.getId());
+
+//        Adding source to customer
+            a.getSources().create(source);
 
 //        charging a customer
-//        Map<String, Object> ChargeParams = new HashMap<>();
-//        ChargeParams.put("amount", 50000);
-//        ChargeParams.put("currency", "usd");
-//        ChargeParams.put("customer", a.getId());
-//
-//        Charge charge = Charge.create(ChargeParams);
-//        System.out.println(charge);
+        Map<String, Object> ChargeParams = new HashMap<>();
+        ChargeParams.put("amount", 50000);
+        ChargeParams.put("currency", "usd");
+        ChargeParams.put("customer", a.getId());
 
-        //retrieve product by price
-//        Product product = Product.retrieve("prod_IrmZPDvM5UiSly");
-//        System.out.println("Product params ->"+product.toJson());
-//        Map<String, Object> prodParams = new HashMap<>();
-//        prodParams.put("plan","price_1IG309EZz34s3JY5HNgDsqqb");//price
-//        prodParams.put("quantity","1");
-////        create item for subs
-//        Map<String, Object> items = new HashMap<>();
-//        items.put("0",prodParams);
-//////        in other to subscribe provide must add the request params eg items
-//        Map<String, Object> subParams = new HashMap<>();
-//        subParams.put("items",items);
-//        subParams.put("customer",a.getId());
-//        subParams.put("off_session", "true");
-////
-//        Subscription cus = Subscription.create(subParams);
-//        System.out.println(cus.toJson());
-//
-////        Creating a payment Intent
-//        Map<String, Object> paymentIntParams = new HashMap<>();
-//        paymentIntParams.put("amount","1000");
-//        paymentIntParams.put("currency","usd");
-//        paymentIntParams.put("customer",a.getId());
-//
-//
-//        PaymentIntent paymentIntent = PaymentIntent.create(paymentIntParams);
-//
-//        System.out.println(paymentIntent.toJson());
+        Charge charge = Charge.create(ChargeParams);
+        System.out.println(charge);
 
-        //        Creating a setup Intent
-//        Map<String, Object> setUpParams = new HashMap<>();
-//        setUpParams.put( "usage", "off_session");
-//        setUpParams.put( "customer", a.getId());
-//        setUpParams.put("payment_method_data", token.getId());
+//        retrieve product by price
+        Product product = Product.retrieve("prod_IrmZPDvM5UiSly");
+        System.out.println("Product params ->"+product.toJson());
+        Map<String, Object> prodParams = new HashMap<>();
+        prodParams.put("plan","price_1IG309EZz34s3JY5HNgDsqqb");//price
+        prodParams.put("quantity","1");
+//        create item for subs
+        Map<String, Object> items = new HashMap<>();
+        items.put("0",prodParams);
+////        in other to subscribe provide must add the request params eg items
+        Map<String, Object> subParams = new HashMap<>();
+        subParams.put("items",items);
+        subParams.put("customer",a.getId());
+        subParams.put("off_session", "true");
 //
-//        SetupIntent setupIntent =  SetupIntent.create(setUpParams);
-//        System.out.println(setupIntent.toJson());
+        Subscription cus = Subscription.create(subParams);
+        System.out.println(cus.toJson());
 
-//        SetupIntent setUpRetrieved = SetupIntent.retrieve("seti_1IIV8OEZz34s3JY5c9ics0vl");
+//        Creating a payment Intent
+        Map<String, Object> paymentIntParams = new HashMap<>();
+        paymentIntParams.put("amount","1000");
+        paymentIntParams.put("currency","usd");
+        paymentIntParams.put("customer",a.getId());
 
 
-//        System.out.println(setUpRetrieved.toJson());
+        PaymentIntent paymentIntent = PaymentIntent.create(paymentIntParams);
+
+        System.out.println(paymentIntent.toJson());
+
+//                Creating a setup Intent
+        Map<String, Object> setUpParams = new HashMap<>();
+        setUpParams.put( "usage", "off_session");
+        setUpParams.put( "customer", a.getId());
+        setUpParams.put("payment_method_data", token.getId());
+
+        SetupIntent setupIntent =  SetupIntent.create(setUpParams);
+        System.out.println(setupIntent.toJson());
+
+        SetupIntent setUpRetrieved = SetupIntent.retrieve("seti_1IIV8OEZz34s3JY5c9ics0vl");
+
+
+        System.out.println(setUpRetrieved.toJson());
 
         PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
                 .setCustomer("cus_Iu6lPq3py40Ds8")
@@ -104,8 +108,8 @@ public class Main {
                 .setCurrency("usd")
                 .build();
 
-        PaymentIntent paymentIntent = PaymentIntent.create(params);
-        System.out.println("payment intent created -> "+paymentIntent.toJson());
+        PaymentIntent intent1 = PaymentIntent.create(params);
+        System.out.println("payment intent created -> "+intent1.toJson());
 
 
 //        SetupIntentConfirmParams
